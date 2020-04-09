0f466c07fb0cdab701611cf6eff8ffa79d158e30
Christophe Schmit <schmit.christophe@hotmail.com>
2020-04-09 18:12:22 +0200

    no message

---
+++ Speed Coding/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Speed_Coding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
