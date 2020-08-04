4fd4cbd8352403ac91c612c42dbd5030121ce5d7
Christophe Schmit <schmit.christophe@hotmail.com>
2020-08-04 16:24:30 +0200

    no message

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Speed_Coding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[100];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = i+1;
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if(((numbers[i] % 3) == 0)&&((numbers[i] % 5)==0))
+                {
+                    numbers[i] = 12121;
+                }
+                else if((numbers[i] % 3) == 0)
+                {
+                    numbers[i] = 22222;
+                }
+                else if((numbers[i] % 5) == 0)
+                {
+                    numbers[i] = 11111;
+                }
+
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
