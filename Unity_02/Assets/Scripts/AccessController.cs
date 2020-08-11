e3a8ca47a9c9971b85336ebb3cb61627632e0ff7
Christophe Schmit <schmit.christophe@hotmail.com>
2020-08-11 18:08:12 +0200

    Unity Project

---
+++ Unity_02/Assets/Scripts/AccessController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+[DefaultExecutionOrder(-50)]
+public class AccessController : MonoBehaviour
+{
+    public const int MAX_ACCESS_PER_FRAME = 3;
+    public static AccessController Instance { get; private set; }
+
+    public int FrameAccess
+    {
+        get
+        {
+            return m_frameAccess;
+        }
+    }
+
+    private int m_frameAccess;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+
+    private void Update()
+    {
+        m_frameAccess = 0;
+    }
+
+    public void AddAccess()
+    {
+        m_frameAccess++;
+    }
+
+    public bool CanAccess()
+    {
+        return m_frameAccess <= MAX_ACCESS_PER_FRAME;
+    }
+}
---
+++ Unity_02/Assets/Scripts/Enemy.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+[DefaultExecutionOrder(-100)]
+[DisallowMultipleComponent]
+public class Enemy : MonoBehaviour
+{
+    public float CurrentHP
+    {
+        get
+        {
+            AccessController.Instance.AddAccess();
+            if (!AccessController.Instance.CanAccess())
+                return -1;
+            return m_currentHP;
+        }
+        private set
+        {
+            m_currentHP = Mathf.Max(0, value);
+        }
+    }
+
+    private float m_currentHP;
+    private int m_depletionRate;
+
+    private void Awake()
+    {
+        m_currentHP = 100;
+        m_depletionRate = Random.Range(3, 10);
+    }
+
+    private void Update()
+    {
+        m_currentHP -= m_depletionRate * Time.deltaTime;
+    }
+
+    private void LateUpdate()
+    {
+        if (m_currentHP <= 0)
+        {
+            Debug.LogError("Die HP des Gegners waren 0, er wurde aber nicht zerstört!", this);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (m_currentHP > 0)
+        {
+            Debug.LogError($"Die HP des Gegners waren nicht 0, als er zerstört wurde!", this);
+        }
+    }
+}
